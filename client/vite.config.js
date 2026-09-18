9c15cda7fe903bcbd610d90c30313520991e5521
luckybamhore-glitch <luckybamhore@gmail.com>
2026-09-18 14:04:11 +0530

    vite host updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5173,
     proxy: {
       '/api': 'http://localhost:5000',
     },
+  },
+
+  preview: {
+    allowedHosts: ['newtodo-fwni.onrender.com'],
   },
 });
